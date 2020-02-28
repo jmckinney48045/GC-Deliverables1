c14f45ae48bf194558125341484bf72940717b75
Jessica <jmckinney48045@gmail.com>
2020-02-28 09:50:11 -0500

    complete deliverable one for unit one

---
+++ source/repos/Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What kind of vacation would you like to go on, musical, tropical, or adventurous? ");
+            string vacationType = Console.ReadLine();
+
+            Console.Write("How many are in your group? ");
+            int groupSize = int.Parse(Console.ReadLine());
+
+            string vacationDest = "";
+            string travelSugg = "";
+
+            if (groupSize <= 2)
+                travelSugg = "first class";
+            if (groupSize >= 3 && groupSize <= 5)
+                travelSugg = "helicopter";
+            if (groupSize >= 6)
+                travelSugg = "charter flight";
+
+            switch (vacationType)
+            {
+                case "tropical": vacationDest = ("a beach vacation in Mexico"); break;
+                case "musical": vacationDest = ("New Orleans"); break;
+                case "adventurous": vacationDest = ("go whitewater rafting on the Grand Canyon"); break;
+
+            }
+
+            string result = "Since you have a group of " + groupSize + " and would like a(n) " + vacationType + " type of vacation, "
+                             + "you should travel by " + travelSugg + " to " + vacationDest + ".";
+           
+            Console.WriteLine(result);
+
+
+        }
+
+
+    }
+
+}
